58cd5932cfbf940e79191c87e4901d241e56211e
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2016-10-27 00:43:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function (req, res) {
     //articleName == article-One
     //articles[articleName] == {} content object for article-One
-    var articleName =req.parans.articleName;
+    var articleName =req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 
