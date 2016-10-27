d91bab8cab75a52794cf48d6c2cadb298d2aaa78
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2016-10-27 16:02:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,7 @@ var marginLeft = 0;
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
  }
+ 
 img.onclick = function () {
    var interval = setInterval(moveRight, 100);
 };
