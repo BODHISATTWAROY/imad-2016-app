3d9af02b73cb86e97fb480765e55fdf514477c95
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2016-10-27 15:27:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML = "New Value";
 //Move the Image
 var img = document.getElementById ("madi");
 img.onclick = function () {
-    img.style.marginLeft ='200px';
+    img.style.marginLeft ='100px';
 };
