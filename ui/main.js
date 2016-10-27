9981ccc240ad311995b18e1439eb15347e47de03
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2016-10-27 15:12:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of main-text div
 var element = getElementById ("main-text");
 element.innerHTML = "New Value";
+
+//Move the Image
+var img = document.getElementById ("img");
+img.onclick = function () {
+    img.style.marginLeft ="100px";
+};
