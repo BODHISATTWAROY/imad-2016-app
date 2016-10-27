72aa24a4f928c17805f1a92264c708b4a051b969
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2016-10-27 15:17:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = getElementById ("main-text");
 element.innerHTML = "New Value";
 
 //Move the Image
-var img = document.getElementById ("img");
+var img = document.getElementById ('img');
 img.onclick = function () {
     img.style.marginLeft ="100px";
 };
